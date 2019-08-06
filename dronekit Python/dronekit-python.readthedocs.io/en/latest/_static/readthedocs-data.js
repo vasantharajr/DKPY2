f189d23f207936bf42044ff3fccd198c3fe5436e
Vasantharaj <mail2vasantharaj@gmail.com>
2019-08-06 20:45:12 +0530

    dkpy2
    drone kit

---
+++ dronekit Python/dronekit-python.readthedocs.io/en/latest/_static/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "dronekit-python",
+    version: "latest",
+    language: "en",
+    programming_language: "words",
+    subprojects: {},
+    canonical_url: "https://dronekit-python.readthedocs.io/en/latest/",
+    theme: "sphinx_3dr_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "91c147fa",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: null
+};
+
